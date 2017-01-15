f123372505c2c18052cfacdb16db884203992ddf
João Lopes e Silva <jpedro.e.silva@gmail.com>
2017-01-15 11:55:11 +0100

    Upload of the main files

---
+++ src/jojothegame.js
@@ -0,0 +1,141 @@
+// JOJO THE GAME
+
+// API for facebook-chat-api
+var login = require("facebook-chat-api");
+// Implements a map of letters with accents to letters without accents
+var prompt = require('prompt');
+// Implements a map of letters with accents to letters without accents
+var af = require('./accent-fold.js');
+// Parse the db of jojos
+var jj = require('./jojothegame_db.js')
+var jojoDB = jj.db;
+
+
+prompt.start();
+
+prompt.get(['username', 'password'], function (err, result) {
+if (err) { return console.error(err) }
+
+var game = new Object();
+game.onStatus = false;
+game.scoreboard = new Object();
+
+login({email: result.username, password: result.password}, function callback (err, api) {
+    if(err) return console.error(err);
+ 	
+ 	// Enable listening to yourself
+ 	api.setOptions({selfListen: true});
+ 	// Enable listening to events
+ 	api.setOptions({listenEvents: true})
+
+	api.getFriendsList(function(err, friendsList) {
+		if(err) return console.error(err);
+		allowedUsers_init = [
+						"Diogo Almeida", 
+						"David Nogueira", 
+						"Tomás Ferreirinha", 
+						"Sebastião Miranda",
+						"Francisco Gaspar",
+						"Bernardo Lemos Caldas"
+						];
+
+		allowedUsers = getAllowedUsersID (api, allowedUsers_init, friendsList, true);
+		console.log("no of allowed users = " + allowedUsers.ID.length)
+	});
+
+
+    api.listen(function callback(err, event) {
+    	if(event.type = "message"){
+	    	if(event.body != undefined){
+	    		var indexID = allowedUsers.ID.indexOf(event.senderID);
+	    		if (indexID != -1){
+	    			if (event.body == "jojothegame" && game.onStatus == false){
+	    				api.sendMessage("Hi " + allowedUsers.name[indexID] + "! You have activated JOJO THE GAME. The main purpose of the game is to find all the jojos in this chat world. All of them are unique and have some distinctive properties. \nYou will be notified whenever you find a jojo (even if you just use it in a sentence) and a scoreboard will be kept to see who finds the most jojos. When all the jojos are found, the game will stop. \n\nFor more info type \"jojothegame help\".", 
+			    			event.threadID)
+	    				game.onStatus = true;
+	    			} else if (game.onStatus == true) {
+		    			if (event.body == "jojothegame" && game.onStatus == true) {
+		    				api.sendMessage("Hi " + allowedUsers.name[indexID] + "! JOJO THE GAME is already active in this chat. \n\n For more info type \"jojothegame help\".", 
+				    			event.threadID)
+		    			} else if (event.body == "jojothegame help") {
+		    				api.sendMessage("To have different commands be recognized please precede them by the keyword \"jojothegame\". This way, unnecessary spam is avoided. The list of commands is:\n    help - Displays all the commands\n    scoreboard - Displays the current scoreboard\n    history [name_of_jojo] - Displays the history related to that jojo\n", 
+				    			event.threadID)
+		    			} else if (event.body == "jojothegame scoreboard") {
+		    				var numCaught = 0;
+		    				var message = [];
+		    				for (var k in game.scoreboard){
+	    					    if (game.scoreboard.hasOwnProperty(k)) {
+							        message += 'Name: ' + k + '-- Score: ' + game.scoreboard[k] + "\n";
+							        numCaught += game.scoreboard[k];
+							    }
+		    				}
+		    				api.sendMessage(message + numCaught +" jojos have been caught. " + (jojoDB.length - numCaught) + " still remaining.",
+		    					event.threadID);
+		    			} else if (event.body.search(/^jojothegame history/) > -1) {
+		    				var jojoFound = false;
+		    				for(i = 0; i < jojoDB.length; i++){
+		    					if(event.body.substring("jojothegame history ".length) == jojoDB[i].name){
+		    						api.sendMessage(jojoDB[i].name + ", " + jojoDB[i].description + ". " + jojoDB[i].history,
+		    							event.threadID);
+		    						jojoFound = true;
+		    						break;
+		    					}
+		    				}
+		    				if (jojoFound = false){
+		    					api.sendMessage("Sorry this jojo is not a valid jojo.",
+		    							event.threadID);
+		    				}
+		    			} else if (af.accent_fold(event.body).search(/jojo/i) > -1) {
+		    				index_start = af.accent_fold(event.body).search(/jojo/i);
+		    				for(i = 0; i < jojoDB.length; i++){
+		    					if(event.body.substring(index_start, index_start+4) == jojoDB[i].name && !jojoDB[i].isCaught){
+		    						api.sendMessage("Congratulations, " + allowedUsers.name[indexID] + "! You have caught " + jojoDB[i].name + ", " + jojoDB[i].description +".", 
+				    					event.threadID);
+		    						jojoDB[i].isCaught = true;
+		    						jojoDB[i].caughtBy = allowedUsers.name[indexID];
+		    						if (!game.scoreboard.hasOwnProperty(allowedUsers.name[indexID])){
+		    							game.scoreboard[allowedUsers.name[indexID]] = 1;
+		    						} else {
+		    							game.scoreboard[allowedUsers.name[indexID]] +=1;
+		    						}
+		    					}
+		    				}
+		    			}
+	    			}
+		    	}
+		    }
+		}
+    });
+});
+});
+
+function getAllowedUsersID (api, allowedUsers, friendsList, includeSelf) {
+	allowedUsersID = [];
+	allowedUsersFullName = [];
+	for (i = 0; i < friendsList.length; i++){
+		for (j = 0; j < allowedUsers.length; j++){
+			if (friendsList[i].fullName == allowedUsers[j]){
+				allowedUsersID.push(friendsList[i].userID);
+				allowedUsersFullName.push(friendsList[i].fullName);
+			}
+		}
+	}
+	if (includeSelf == true){
+		allowedUsersID.push(api.getCurrentUserID());
+		allowedUsersFullName.push("jojo");
+	}
+
+	return {
+		ID: allowedUsersID,
+		name: allowedUsersFullName
+	}
+};
+
+function isInArray(arr,obj) {
+    return (arr.indexOf(obj) != -1);
+}
+
+
+
+
+
---
+++ src/jojothegame_db.js
@@ -0,0 +1,253 @@
+//JOJODEX
+
+var jojoDB = 
+[{  "name":"jojo", 
+	"description":"the one and only", 
+	"upEvo":"JOJO", 
+	"downEvo":"", 
+	"history": "Retrieved from an ancient e-mail, this jojo is the most common of jojos.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "JOJO", 
+	"description":"the bigger version of the original",
+	"upEvo":"", 
+	"downEvo":"jojo", 
+	"history": "It's the same but much more than the original. On steroids.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "JOjo", 
+	"description":"where the first is bigger them the last",
+	"upEvo":"joJO", 
+	"downEvo":"", 
+	"history": "An assymetric being prone to depression bouts.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "joJO", 
+	"description":"where the last is bigger them the first",
+	"upEvo":"", 
+	"downEvo":"JOjo", 
+	"history": "An assymetric being prone to extreme elation.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "JOJo", 
+	"description":"it looks like a typo but it is still valid",
+	"upEvo":"jojO", 
+	"downEvo":"", 
+	"history": "It has a very small eye.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jojO", 
+	"description":"it looks like a typo but it is still valid",
+	"upEvo":"", 
+	"downEvo":"JOJo", 
+	"history": "It has a very big eye.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jOJo", 
+	"description":"who could be orange juice but it ain't",
+	"upEvo":"JojO", 
+	"downEvo":"", 
+	"history": "\"I'm absolutely, 100 percent, not guilty.\"",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "JojO", 
+	"description":"likes extremities a lot",
+	"upEvo":"JojO", 
+	"downEvo":"", 
+	"history": "The criminal record of this jojo is confidential as he is under investigation.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "ǰojo", 
+	"description":"half-Croatian jojo",
+	"upEvo":"ǰoǰo", 
+	"downEvo":"", 
+	"history": "Born and raised in the mean streets of Zagreb, this jojo had a tough childhood.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "ǰoǰo", 
+	"description":"the thug of Zagreb",
+	"upEvo":"", 
+	"downEvo":"ǰojo", 
+	"history": "Lurks the streets of Zagreb working for his Croatian family.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jǒjo", 
+	"description":"half-Serbian jojo. Do not confuse with the Croation jojos.",
+	"upEvo":"jǒjǒ", 
+	"downEvo":"", 
+	"history": "Born and raised in the mean streets of Belgrade, this jojo had a tough childhood.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jǒjǒ", 
+	"description":"the thug of Belgrade",
+	"upEvo":"", 
+	"downEvo":"jǒjo", 
+	"history": "Lurks the streets of Belgrade working for his Serbian family. Has a strong dislike for all the Croation jojos.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jojő", 
+	"description":"the Magyar",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "Proud and estoic. Enjoys long strolls in Sziget.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+
+{ 	"name": "jøjo", 
+	"description":"the blonde beauty",
+	"upEvo":"jøjø", 
+	"downEvo":"", 
+	"history": "Strolls the streets of Oslo. Thinks no one suspects that is dyed.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jøjø", 
+	"description":"the blonde and tanned gorgeousness. Basically the pewdiepie equivalent in jojo form",
+	"upEvo":"", 
+	"downEvo":"jøjo", 
+	"history": "He even responds by Felix.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jƟjo", 
+	"description":"who studied dialectic and rethorics",
+	"upEvo":"JƟJƟ", 
+	"downEvo":"", 
+	"history": "Studied under an even more powerful master of philosophy.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "JƟJƟ", 
+	"description":"the Philosopher",
+	"upEvo":"", 
+	"downEvo":"jƟjo", 
+	"history": "He was actually egyptian but thought being known as JƟJƟ had a little more flair to it.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jojȯ", 
+	"description":"who is sleeping like a Livonian",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "Has difficulty understand why his tongue gets numb when he speaks a dormant language.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+
+{ 	"name": "jǪjǪ", 
+	"description":"the distorted proletarian in Katowice",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "Some say the difference in case was due to a sloppy notary.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+
+{ 	"name": "jojő", 
+	"description":"the Magyar",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "Proud and estoic. Enjoys long strolls in Sziget.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "JŏJŏ", 
+	"description":"a Bucarest solitary monk",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "He does not talk much as he took a vow.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "Jojơ", 
+	"description":"the Napalm Warrior",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "No, he doesn't love the smell of it.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+
+{ 	"name": "jõjõ", 
+	"description":"who is nosy as fuck",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "He has some problems with his sinuses.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "ĵojo", 
+	"description":"who dreams of a united language",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "ĵojo estas la mastro de ĉiuj el ni",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+
+{ 	"name": "joɉo", 
+	"description":"just like in just",
+	"upEvo":"", 
+	"downEvo":"", 
+	"history": "He lives in English dictionaries.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jojô", 
+	"description":"diretamente do Brasil",
+	"upEvo":"jôjô", 
+	"downEvo":"", 
+	"history": "Originary from Brazil and from some spelling correctors, this jojo appears often in group chats.",
+	"isCaught": false,
+	"caughtBy": ""
+},
+
+{ 	"name": "jôjô", 
+	"description":"\"MOROOO NUM PAIS TROPICAAAL, ABENÇOADO POR DEEEUS!\"",
+	"upEvo" : "", 
+	"downEvo":"jojô", 
+	"history": "\"E bonito por natureza / (Mas que beleza) / Em fevereiro / (Em fevereiro), tem carnaval / (Tem carnaval)\".",
+	"isCaught": false,
+	"caughtBy": ""
+},
+];
+
+exports.db = jojoDB;
